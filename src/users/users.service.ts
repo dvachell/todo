5d3a347d95b0dd823576f730c91ee6381273a722
Lev <91049366+dvachell@users.noreply.github.com>
2025-09-23 22:59:04 +0200

    dto added

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,16 +1,22 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaService } from 'src/prisma/prisma.service';
+import { PrismaService } from '../prisma/prisma.service';
 import { User } from '@prisma/client';
+import { CreateUserDto } from './dto/create-user.dto';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
 export class UsersService {
   constructor(private prisma: PrismaService) {}
 
-  async findAll(): Promise<User[]> {
-    return this.prisma.user.findMany();
-  }
+  async createUser(data: CreateUserDto): Promise<User> {
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(data.password, salt);
 
-  async createUser(data: { name: string; email: string }): Promise<User> {
-    return this.prisma.user.create({ data });
+    return this.prisma.user.create({
+      data: {
+        email: data.email,
+        password: hashedPassword,
+      },
+    });
   }
 }

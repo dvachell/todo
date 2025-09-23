7acc1969b5636306de819454e0704831f686f4a2
Lev <91049366+dvachell@users.noreply.github.com>
2025-09-23 19:30:15 +0200

    project init

---
+++ src/users/users.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { User } from '@prisma/client';
+
+@Injectable()
+export class UsersService {
+  constructor(private prisma: PrismaService) {}
+
+  async findAll(): Promise<User[]> {
+    return this.prisma.user.findMany();
+  }
+
+  async createUser(data: { name: string; email: string }): Promise<User> {
+    return this.prisma.user.create({ data });
+  }
+}

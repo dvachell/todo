69ef6beb5924f7852d9c1772530a2824b0eff769
dvachell <skyfin200517@gmail.com>
2025-09-24 19:16:09 +0200

    dto

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,12 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
-  @Get()
-  async findAll(): Promise<any> {
-    return this.usersService.findAll();
+  @Post('register')
+  async register(@Body() createUserDto: CreateUserDto) {
+    return this.usersService.createUser(createUserDto);
   }
 }

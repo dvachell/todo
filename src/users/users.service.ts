f72d04e4a62f3f3d6d74b3564a5c1c77f08fef4e
dvachell <skyfin200517@gmail.com>
2025-09-29 20:05:26 +0200

    is email exist check added

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { User } from '@prisma/client';
 import { CreateUserDto } from './dto/create-user.dto';
+import { LoginUserDto } from './dto/login-user.dto';
 import * as bcrypt from 'bcryptjs';
 
 @Injectable()
@@ -9,6 +10,14 @@ export class UsersService {
   constructor(private prisma: PrismaService) {}
 
   async createUser(data: CreateUserDto): Promise<User> {
+    const existingUser = await this.prisma.user.findUnique({
+      where: { email: data.email },
+    });
+    
+    if(existingUser) {
+      throw new ConflictException('User with this email already exists');
+    }
+
     const salt = await bcrypt.genSalt();
     const hashedPassword = await bcrypt.hash(data.password, salt);
 
@@ -18,5 +27,12 @@ export class UsersService {
         password: hashedPassword,
       },
     });
+  }
+
+  async validateUser(data: LoginUserDto): Promise<User | null> {
+    const user = await this.prisma.user.findUnique({
+      where: { email: data.email },
+    });
+    return user // ???
   }
 }
